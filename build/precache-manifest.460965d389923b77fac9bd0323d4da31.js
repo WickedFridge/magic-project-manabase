8b8565fea7f469fc45e6dd755383c74a58bcd113
Charles Wickham <charles.wickham.consulting@gmail.com>
2020-04-06 18:31:47 +0200

    feat(input): handling MTGA Exports + last build

--- build/precache-manifest.460965d389923b77fac9bd0323d4da31.js
+++ build/precache-manifest.460965d389923b77fac9bd0323d4da31.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9290ed03d897c9ac9078cfcf180b7ac",
+    "revision": "255cf7bcc609d3c747e29340742112fa",
     "url": "/index.html"
   },
   {
-    "revision": "58abafc2935036932905",
+    "revision": "f9f17cfa943269936f47",
     "url": "/static/css/main.328afb4b.chunk.css"
   },
   {
-    "revision": "fa5eff7c91a04419a71c",
-    "url": "/static/js/2.d64471f0.chunk.js"
+    "revision": "63604a0a2b40d0f6b037",
+    "url": "/static/js/2.c336312c.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.d64471f0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c336312c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58abafc2935036932905",
-    "url": "/static/js/main.44b98adc.chunk.js"
+    "revision": "f9f17cfa943269936f47",
+    "url": "/static/js/main.0159ae84.chunk.js"
   },
   {
     "revision": "5109afe555ccf31541a7",
