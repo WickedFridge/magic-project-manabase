20aa477c3bdf9db91b6062d03b6de2de0788418b
Charles Wickham <charles.wickham.consulting@gmail.com>
2020-04-10 18:33:04 +0200

    next build

---
+++ build/precache-manifest.bda1bfc299b89a856ea44368c80ccee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724c0f2db8198fdf2cde93fd044de975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfe8fa4e72ea82e475e3",
+    "url": "/static/css/main.328afb4b.chunk.css"
+  },
+  {
+    "revision": "63604a0a2b40d0f6b037",
+    "url": "/static/js/2.c336312c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c336312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe8fa4e72ea82e475e3",
+    "url": "/static/js/main.3bfde4cc.chunk.js"
+  },
+  {
+    "revision": "5109afe555ccf31541a7",
+    "url": "/static/js/runtime-main.3623e3b0.js"
+  }
+]);
